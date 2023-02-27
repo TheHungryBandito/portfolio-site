7a62ed805aa0987da427748d8bcf1de1769d1e9b
TheHungryBandito <joshuacrews16@outlook.com>
2023-02-27 22:33:37 +1000

    Customized theme
    - Divided content into sections
    - Added SVG files that help with transitions
    - Started ProjectCard.vue which holds an individual project
    - Added Project section
    - Added social logos
    - Changed layout and theme

--- Portfolio-Site/src/App.vue
+++ Portfolio-Site/src/App.vue
@@ -1,25 +1,32 @@
 <template>
-  <MainHeader 
-  title="Joshua Crews" 
-  position="Generalist Software developer" 
-  tools="C#/.NET, Python, JavaScript" 
-  description="Freelancer, available for work"
-  location="Based in Australia"/>
-  
-  <SocialLinks 
-  linkedInURL="https://www.linkedin.com/in/joshua-crews-hb23/" 
-  gitHubURL="https://github.com/TheHungryBandito"/>
+  <section class="spacer layer2">
+    <ProfileCard title="Joshua Crews" position="Generalist Software developer" tools="C#/.NET, Python, JavaScript"
+      description="Freelancer, available for work" location="Based in Australia" />
+
+    <div id="social-links-container">
+      <SocialLink name="LinkedIn" url="https://www.linkedin.com/in/joshua-crews-hb23/"
+        imageSrc="./assets/In-White-40-┬«.png" />
+      <SocialLink name="GitHub" url="https://github.com/TheHungryBandito" imageSrc="./assets/In-White-40-┬«.png" />
+    </div>
+  </section>
+  <section class="darkblue">
+    <h1>Projects</h1>
+    <ProjectCard projectName="Porfolio-Site" role="Sole Developer" projectDescription="Site to hold my portfolio." />
+  </section>
+  <section class="spacer layer5">
+    <h1 style="margin-top: 60%">Section 3</h1>
+  </section>
 </template>
 
 <script>
-import MainHeader from './components/MainHeader.vue';
-import SocialLinks from './components/SocialLinks.vue';
+import ProfileCard from './components/ProfileCard.vue';
+import ProjectCard from './components/ProjectCard.vue';
 
 export default {
   name: 'App',
   components: {
-    MainHeader,
-    SocialLinks,
+    ProfileCard,
+    ProjectCard,
   }
 }
 </script>
---
+++ Portfolio-Site/src/components/ProjectCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <div id="project-container">
+            <img :src="projectThumbnail" alt="No Image Available">
+            <h2>{{ projectName }}</h2>
+            <h3>{{ role }}</h3>
+            <p>{{ projectDescription }}</p>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'ProjectCard',
+    props: {
+        projectName: String,
+        projectDescription: String,
+        projectThumbnail: String,
+        role: String,
+        achievements: String,
+        url: String,
+    }
+}
+</script>
