37cb630b6fce0d49fb5e23e939e2ecbcb96f1dda
TheHungryBandito <joshuacrews16@outlook.com>
2023-02-27 14:21:19 +1000

    Divided website into .vue components
    - Removed template .vue files
    - Moved content added content from index.html into .vue components
    - Added MainHeader.vue for name and brief info of the portfolio user
    - Added SocialLinks.vue for social links such as github and linked in.
    - MainHeader and social links get info from App.vue

---
+++ Portfolio-Site/src/components/MainHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header>
+        <div id="title-container">
+            <h1 id="title">{{ title }}</h1>
+            <h2 id="subtitle">{{ position }}</h2>
+            <h2 id="subtitle">{{ tools }}</h2>
+            <p id="text">
+                {{ description }}
+                <br>
+                {{ location }} 
+            </p>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'MainHeader',
+    props: {
+        title: String,
+        position: String,
+        tools: String,
+        description: String,
+        location: String,
+    }
+}
+</script>
