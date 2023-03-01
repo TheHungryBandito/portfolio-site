d5169f762f429791d710470756a40805385fc620
TheHungryBandito <joshuacrews16@outlook.com>
2023-03-01 12:50:35 +1000

    Made some elements resizable based on screen size
    - Project cards now have 2 fixed sizes for screens less than 1080px width and greater than 1080px width
    - Profile text now resizes dynamically based on screen size.

--- Portfolio-Site/src/App.vue
+++ Portfolio-Site/src/App.vue
@@ -8,7 +8,7 @@
     <div id="project-cards">
       <ProjectCard class="item1 layer1" projectName="Portfolio-Site" role="Sole Developer"
         projectDescription="Made to hold projects created by Joshua Crews, the portfolio site itself was created using Node.JS, JavaScript, HTML, CSS, and Vue.JS with source control using GitHub."
-        projectThumbnailURL="portfolio-site-thumbnail.png" url="http://localhost:5173/" />
+        projectThumbnailURL="portfolio-site-thumbnail.png" url="https://github.com/TheHungryBandito/portfolio-site" />
       <ProjectCard class="item2 layer4" projectName="IT Ticketing System" role="Backend Developer / Mentor"
         projectDescription="(Work in progress) A fully featured ticket system for the IT sector. This project is being built using Node.JS, Express.JS, MySQL, Gulp, HTML, CSS, SCSS."
         projectThumbnailURL="" url="https://github.com/RJD27/it_ticketing_system" />
@@ -16,7 +16,7 @@
         projectDescription="A rogue-lite 2D Tower defense game where towers and abilities are in the form of cards and players can get achievements to unlock more cards. Built with Unity, Shader Graph, C#, GitHub."
         projectThumbnailURL="" url="https://github.com/TheHungryBandito/Tower-Shuffle" />
       <ProjectCard class="item4 layer5" projectName="Social Post Generator" role="Software Developer"
-        projectDescription="A social media post generator that integrates the ChatGPT API and DALLE API to create template social media posts about the user specified topic. Built using Python."
+        projectDescription="(Unavailable) A social media post generator that integrates the ChatGPT API and DALLE API to create template social media posts about the user specified topic. Built using Python."
         projectThumbnailURL="" />
     </div>
   </section>
