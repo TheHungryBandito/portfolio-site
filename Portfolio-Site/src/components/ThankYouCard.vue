97cc5aa0c498cb7e4776254819d17ae5e127e48d
TheHungryBandito <joshuacrews16@outlook.com>
2023-03-07 11:45:09 +1000

    Create ThankYouCard.vue
    - Added Thank you page to appear after contact form is complete.

---
+++ Portfolio-Site/src/components/ThankYouCard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="thank-you-container">
+        <div class="darkblue glow">
+            <h1 class="titlefont">Thank You</h1>
+            <h3>Your contact request has been submitted successfully.</h3>
+        </div>
+        <div>
+            <button class="glow darkblue" @click="home">Home</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ThankYouCard',
+    methods: {
+        home() {
+            this.$router.push({ path: '/' });
+            document.getElementById('app').scrollIntoView({ behavior: 'smooth' });
+        }
+    }
+}
+</script>
